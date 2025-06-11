cd72ecd9fa3e998f9dce3f2b4fa809cbae474b20
Smruti <smrutipadshala11@gmail.com>
2025-06-11 13:41:42 +0530

    Initial Commit

---
+++ frontend/src/Data/TrendData.js
@@ -0,0 +1,27 @@
+export const TrendData= [
+    {
+      name: "LinkUp",
+      shares: 97,
+    },
+    {
+      name: "SocialMedia",
+      shares: 80.5,
+    },
+    {
+      name: "Smruti's App",
+      shares: 75.5,
+    },
+    {
+      name: "Reactjs",
+      shares: 72,
+    },
+    {
+      name: "SKP",
+      shares: 71.9,
+    },
+    {
+      name: "Advanced Technology",
+      shares: 20,
+    },
+  ];
+  
